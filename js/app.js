8b0cc116f3e407935ced594b6ee5aba9d87a91cc
YoshiCity <pawel.mirys@gmail.com>
2021-10-05 21:34:08 +0200

    counting on click

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const counter = document.querySelector('#counter');
+const buttons = document.querySelectorAll('.btn');
+let count = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.classList.contains('prevBtn')) {
+      count--;
+    } else {
+      count++;
+    }
+    counter.textContent = count.toString();
+    if (count > 0) {
+      counter.classList.remove('red');
+      counter.classList.add('green');
+    } else if (count < 0) {
+      counter.classList.remove('green');
+      counter.classList.add('red');
+    } else {
+      counter.classList.remove('green');
+      counter.classList.remove('red');
+    }
+  });
+});
+
+/* const subCount = () => {
+   initCountValue--;
+   counter.textContent = initCountValue.toString();
+   return initCountValue;
+ };
+ const addCount = () => {
+   initCountValue++;
+   counter.textContent = initCountValue.toString();
+   return initCountValue;
+ };
+
+ subBtn.addEventListener('click', subCount);
+ addBtn.addEventListener('click', addCount);
+
+ console.log(counter);
+
+*/
